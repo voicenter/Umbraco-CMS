fdce0545083ec8063c9b164b8c5df59e5b1682f0
Shlomi Gutman <shlomi@voicenter.com>
2018-03-05 14:25:13 +0200

    log4net WebAPI Demo

--- src/Umbraco.Web.UI/Umbraco/Api/ContentController.cs
+++ src/Umbraco.Web.UI/Umbraco/Api/ContentController.cs
@@ -6,13 +6,23 @@ using System.Net.Http;
 using System.Web.Http;
 using umbraco.NodeFactory;
 using Umbraco.Web.WebApi;
+ using log4net;
+using System.Reflection;
 
 namespace Umbraco.Web.UI
 {
     public class ContentController : UmbracoApiController
     {
+        private static readonly ILog Log =
+          LogManager.GetLogger(
+              MethodBase.GetCurrentMethod().DeclaringType
+          );
+
         public Node GetAllProducts(int id = 1103)
         {
+
+            Log.Debug("GetAllProducts !!!!!!!!!!!!!!!!!" + id.ToString()) ;
+
             Node driverParentNode = new Node(id);
             return driverParentNode ;
         }
